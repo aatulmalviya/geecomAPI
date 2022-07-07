54b0ff62485675ce2506dd60e6d22052b02c78f2
Atul <Atul@DESKTOP-8SPUJLU>
2022-07-08 00:32:32 +0530

    dbhelper

---
+++ geecomAPI/dbUtilites/dbHelper.cs
@@ -0,0 +1,356 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace geecomAPI.dbUtilites
+{
+    public class dbHelper
+    {
+        public static DataTable GetTableFromSP(string connectionString, string sp, Dictionary<string, object> parametersCollection)
+        {
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+
+                foreach (KeyValuePair<string, object> parameter in parametersCollection)
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+
+                DataSet dataSet = new DataSet();
+                (new MySqlDataAdapter(command)).Fill(dataSet);
+                command.Parameters.Clear();
+
+                if (dataSet.Tables.Count > 0)
+                {
+                    return dataSet.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return null;
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+        }
+
+        public static DataTable GetTableFromSP(string connectionString, string sp, MySqlParameter[] prms)
+        {
+
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+
+                command.Parameters.AddRange(prms);
+
+                DataSet dataSet = new DataSet();
+                (new MySqlDataAdapter(command)).Fill(dataSet);
+                command.Parameters.Clear();
+
+                if (dataSet.Tables.Count > 0)
+                {
+                    return dataSet.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static DataTable GetTableFromSP(string connectionString, string sp)
+        {
+
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+
+                DataSet dataSet = new DataSet();
+                (new MySqlDataAdapter(command)).Fill(dataSet);
+                command.Parameters.Clear();
+
+                if (dataSet.Tables.Count > 0)
+                {
+                    return dataSet.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return null;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+        }
+
+        public static void ExecuteNonQuery(string connectionString, string sp, MySqlParameter[] prms)
+        {
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+
+                command.Parameters.AddRange(prms);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+        }
+
+        public static void ExecuteNonQuery(string connectionString, string sp, MySqlParameter prms)
+        {
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+                //prms.MySqlDbType = MySqlDbType.Structured;
+                command.Parameters.Add(prms);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+        }
+
+        public static void ExecuteNonQuery(string connectionString, string sp, MySqlParameter prm, MySqlParameter[] prms)
+        {
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+                //prm.MySqlDbType = MySqlDbType.Structured;
+                command.Parameters.Add(prm);
+                command.Parameters.AddRange(prms);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+        }
+
+        public static DataTable GetTableRow(string connectionString, string sp, MySqlParameter[] prms)
+        {
+
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                command.Parameters.AddRange(prms);
+                connection.Open();
+
+                DataSet dataSet = new DataSet();
+                (new MySqlDataAdapter(command)).Fill(dataSet);
+                command.Parameters.Clear();
+
+                if (dataSet.Tables.Count > 0)
+                {
+                    return dataSet.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static DataSet GetDatasetFromSP(string connectionString, string sp, MySqlParameter[] prms)
+        {
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                MySqlCommand command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+
+                command.Parameters.AddRange(prms);
+
+                DataSet dataSet = new DataSet();
+                (new MySqlDataAdapter(command)).Fill(dataSet);
+                command.Parameters.Clear();
+
+                return dataSet;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static int ExecuteNonQueryReturn(string connectionString, string sp, MySqlParameter[] prms)
+        {
+
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+                command.Parameters.AddRange(prms);
+                int result = command.ExecuteNonQuery();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+        }
+
+        public static string ExecuteScalarFunction(string connectionString, string CommandText)
+        {
+            string Result = "";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                connection.Open();
+                command = new MySqlCommand(CommandText, connection);
+                MySqlDataAdapter da = new MySqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                Result = dt.Rows[0][0].ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+
+            return Result;
+
+        }
+
+        public static void ExecuteMultipleDatatable(string connectionString, string sp, MySqlParameter[] prms, DataSet ds)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand();
+            try
+            {
+                command = new MySqlCommand(sp, connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = connection.ConnectionTimeout };
+                connection.Open();
+                command.Parameters.AddRange(prms);
+                if (null != ds)
+                {
+                    foreach (DataTable dt in ds.Tables)
+                    {
+                        MySqlParameter parameter = new MySqlParameter();
+                        //parameter.MySqlDbType = MySqlDbType.Structured;
+
+                        //DataTable.TableName is the parameter Name
+                        //e.g: @AppList
+                        parameter.ParameterName = dt.TableName;
+                        //DataTable.DisplayExpression is the equivalent SQLType Name. i.e. Name of the UserDefined Table type
+                        //e.g: AppCollectionType
+                        //parameter.TypeName = dt.DisplayExpression;
+                        //parameter.TypeName = dt.Namespace;
+                        parameter.ParameterName = dt.Namespace;
+
+                        parameter.Value = dt;
+
+                        command.Parameters.Add(parameter);
+                    }
+                }
+                int result = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+                command.Dispose();
+            }
+
+
+        }
+    }
+}
