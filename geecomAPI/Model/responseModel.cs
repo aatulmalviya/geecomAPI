d1f34e164fc60c37a33b595410f9e97ed013a751
GeeCom <GeeCom@DESKTOP-IBC32MG>
2022-07-08 12:52:16 +0530

    model

--- geecomAPI/Model/responseModel.cs
+++ geecomAPI/Model/responseModel.cs
@@ -1,8 +1,30 @@
-﻿namespace geecomAPI.Model
+﻿using System;
+
+namespace geecomAPI.Model
 {
-    public class responseModel
+    public class responseHeaderModel
     {
+        public int responseHeaderID { get; set; }
+        public int orgID { get; set; }
+        public int farmerID { get; set; }
+        public int questionSetID { get; set; }
+
+        public string location { get; set; }
+        public DateTime addTime { get; set; }
+        public string addUser { get; set; }
+
+        public DateTime? updateTime { get; set; }
+        public string? updateUser { get; set; }
 
 
+    }
+    public class responseDetailModel
+    {
+        public int respID { get; set; }
+        public int responseHeaderID { get; set; }
+        public int questionID { get; set; }
+        public string response { get; set; }
+        public DateTime addTime { get; set; }
+        public string addUser { get; set; }
     }
 }
